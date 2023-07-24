b9b3e2cd0f944022aaf2c27813285f90ed81abdd
Walid Newaz <mdwalid@gmail.com>
2023-07-24 07:37:44 -0600

    Added geo coder compoent to map

--- src/components/MapBox/MapBox.tsx
+++ src/components/MapBox/MapBox.tsx
@@ -1,6 +1,9 @@
 import React, { useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+// import request from 'request';
 
 const MAPBOX_ACCESS_TOKEN: string = process.env.MAPBOX_ACCESS_TOKEN as string;
 
@@ -38,12 +41,47 @@ const MapBox: React.FC = () => {
           showCompass: true,
         }),
       );
+
+      // Add the control to the map.
+      map.current.addControl(
+        new MapboxGeocoder({
+          accessToken: mapboxgl.accessToken,
+          mapboxgl: mapboxgl,
+          reverseGeocode: true,
+        }),
+        'top-left',
+      );
     }
   }, []);
+
+  const getCurrentPosition = () => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+      console.log(position.coords.accuracy);
+
+      const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${long},${lat}.json?access_token=${MAPBOX_ACCESS_TOKEN}`;
+
+      const response = await fetch(url);
+      const features = await response.json();
+      console.log(features);
+    });
+  };
 
   return (
     <div className="flex flex-col h-full">
       <div className="flex-grow" ref={mapContainer}></div>
+      <div className="flex justify-center">
+        <button
+          className="bg-travvit-orange-900 hover:bg-travvit-orange-800 text-white font-bold py-2 px-4 rounded"
+          onClick={getCurrentPosition}
+        >
+          Get Current Position
+        </button>
+      </div>
     </div>
   );
 };
